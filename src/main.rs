8ecfb89ab1bcb04865961ccf6b87737609ba2746
SaadiSave <saadisave@gmail.com>
2022-03-15 10:13:42 +0530

    new(route): health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use std::net::SocketAddr;
+
+async fn health_check() -> hyper::StatusCode {
+    hyper::StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> hyper::Result<()> {
+    let app = Router::new().route("/health_check", get(health_check));
+
+    Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8000)))
+        .serve(app.into_make_service())
+        .await
 }
